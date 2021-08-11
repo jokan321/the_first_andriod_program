6de6271ca5693b3d141ee3c49731f52f0ec839b8
Jokan <1012233094@qq.com>
2021-08-11 21:35:09 +0900

    第八章ソース完了なお第九章ソース新規

--- myslidesshow/app/src/main/java/com/example/myslideshow/ImageFragment.kt
+++ myslidesshow/app/src/main/java/com/example/myslideshow/ImageFragment.kt
@@ -5,40 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myslideshow.databinding.FragmentImageBinding
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 private const val IMG_RES_ID = "IMG_RES_ID"
 
 class ImageFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
     private var imageResId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            imageResId = it.getInt(IMG_RES_ID)
         }
     }
+
+    private var _binding: FragmentImageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image, container, false)
+    ): View {
+        _binding = FragmentImageBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(imageResId: Int) =
             ImageFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(IMG_RES_ID, imageResId)
                 }
             }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        imageResId?.let {
+            binding.imageView.setImageResource(it)
+        }
     }
 }
